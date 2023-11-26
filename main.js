3f6b7abb5ae6a2dc21d03870323692841454515b
Ayman-K-Siddiqui <ayman.siddiqui21@gmail.com>
2023-11-26 01:00:28 -0500

    Books can be added by user

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function Book(title, author, pages, haveRead) {
 
   this.info = function () {
     if (this.haveRead) {
-      return `${this.title} by ${this.author}, ${this.pages} pages, have read the book`;
+      return `${this.title} by ${this.author}, ${this.pages}, Yes`;
     } else {
-      return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
+      return `${this.title} by ${this.author}, ${this.pages}, No`;
     }
   };
 }
@@ -20,17 +20,49 @@ function addBookToLibrary(title, author, pages, haveRead) {
   myLibrary.push(newBookObject);
 }
 
-addBookToLibrary("your lie", "william", 280, true);
-addBookToLibrary("your lie", "william", 280, true);
-addBookToLibrary("your lie", "william", 280, true);
-addBookToLibrary("your lie", "william", 280, true);
+// Manual Additions
+// addBookToLibrary("Book", "Author", 290, false);
+// addBookToLibrary("your lie", "william", 280, true);
+// addBookToLibrary("your lie", "william", 280, true);
+// addBookToLibrary("your lie", "william", 280, true);
 
+// Displays Books
 function displayLibraryBooks(myLibrary) {
   const display = document.getElementById("displayBooks");
 
   myLibrary.forEach((element) => {
-    display.innerHTML += `<p>${element.info()}</p>`;
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+    bookDiv.innerHTML = `<p>${element.info()}</p>`;
+    display.appendChild(bookDiv);
   });
 }
 
 displayLibraryBooks(myLibrary);
+
+const showButton = document.getElementById("showDialog");
+const addDialog = document.getElementById("addDialog");
+const cancelButton = document.getElementById("cancelBtn");
+const submitButton = document.getElementById("submitBtn");
+
+showButton.addEventListener("click", () => {
+  addDialog.showModal();
+});
+
+cancelButton.addEventListener("click", () => {
+  addDialog.close();
+});
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault(); // prevent the default form submission behavior
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const haveRead = document.getElementById("read").value === "yes";
+
+  console.log(title, author, pages, haveRead);
+  addBookToLibrary(title, author, pages, haveRead);
+  displayLibraryBooks(myLibrary);
+  addDialog.close();
+});
